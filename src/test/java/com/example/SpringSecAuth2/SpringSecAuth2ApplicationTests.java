0b4dac835034f7d800d7581f21c8f367eaac86a5
connor999jobs <coonor999jobs@gmail.com>
2021-12-31 15:24:04 +0200

    Initial commit

---
+++ src/test/java/com/example/SpringSecAuth2/SpringSecAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
